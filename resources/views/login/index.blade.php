e3aec774457a19a9be96484ad790ca153c718fef
rifqihardiyanto <rifqihardiyanto23@gmail.com>
2023-07-31 16:39:00 +0700

    first commit

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sign In</title>
+    <link rel="stylesheet" href="css/sign-in.css">
+  </head>
+  <body>
+    <div class="tengah">
+      <h1>Login</h1>
+      <form method="post">
+        <div class="txt_field">
+          <input type="text" required>
+          <span></span>
+          <label>Username</label>
+        </div>
+        <div class="txt_field">
+          <input type="password" required>
+          <span></span>
+          <label>Password</label>
+        </div>
+        <div class="password">Forgot Password?</div>
+        <input type="submit" value="Login">
+        <div class="link_register">
+          Not a member? <a href="#">Signup</a>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
